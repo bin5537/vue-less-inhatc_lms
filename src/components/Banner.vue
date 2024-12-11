03a73372f2be6c148a59329399b79e6b3e167aef
soobin5537 <soobin5537@gmail.com>
2024-12-11 14:14:30 +0900

    v-for, 배열구조 업데이트
    v-for, 데이터 배열 분리

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,22 +1,22 @@
 <template>
-    <section id="banner" ref="bannerContainer">
+    <section id="banner" ref="bannerContainer" :class="{ close: isClosed }">
         <article class="inner">
             <h1>
-                <span>1학년 B반</span>
-                <span>데이터사이언스기초</span>
+                <span>{{ classInfo.grade }} {{ classInfo.class }}</span>
+                <span>{{ classInfo.subject }}</span>
             </h1>
             <div class="content">
                 <div class="tInfo">
                     <span>
-                    <span>이수구분 <span>교양학과</span></span>
-                    <span>학점 <span>2학점</span></span>
-                    <span>공동교수정보 <span class="btn">조회하기</span></span>
+                        <span>이수구분 <span>{{ classInfo.category }}</span></span>
+                        <span>학점 <span>{{ classInfo.credit }}</span></span>
+                        <span>공동교수정보 <span class="btn">조회하기</span></span>
                     </span>
                 </div>
                 <div class="tInfo">
                     <span>
-                    <span>대표교수 <span>이X땡</span></span>
-                    <span>이메일 <span>bibibi@inhatc.ac.kr</span></span>
+                        <span>대표교수 <span>{{ classInfo.professor }}</span></span>
+                        <span>이메일 <span>{{ classInfo.email }}</span></span>
                     </span>
                 </div>
             </div>
@@ -28,15 +28,24 @@
     </section>
 </template>
 
-<script>
+<script setup>
 
-export default {
-    methods: {
-        bannerToggle() {
-            const bannerContainer = this.$refs.bannerContainer;
-            bannerContainer.classList.toggle("close");
-        }
-    }
-}
+import { ref } from "vue";
+
+const classInfo = {
+    grade: "1학년",
+    class: "B반",
+    subject: "데이터사이언스기초",
+    category: "교양학과",
+    credit: "2학점",
+    professor: "이X땡",
+    email: "bibibi@inhatc.ac.kr",
+};
+
+const isClosed = ref(false);
+
+const bannerToggle = () => {
+    isClosed.value = !isClosed.value;
+};
 
 </script>
--- src/components/Content.vue
+++ src/components/Content.vue
@@ -11,50 +11,10 @@
                 <div class="bar"></div>
                 <div class="list">
                     <ul>
-                        <li>
-                            <span>학습 목차</span>
+                        <li v-for="(menu, index) in menuItems" :key="index">
+                            <span>{{ menu.title }}</span>
                             <ul>
-                                <li>학습 목차</li>
-                                <li>강의 목록</li>
-                            </ul>
-                        </li>
-                        <li>
-                            <span>학습 정보</span>
-                            <ul>
-                                <li>공지사항</li>
-                                <li>질의응답</li>
-                                <li>자료공유</li>
-                                <li>상담게시판</li>
-                            </ul>
-                        </li>
-                        <li>
-                            <span>학습 활동</span>
-                            <ul>
-                                <li>출석</li>
-                                <li>과제</li>
-                                <li>토론</li>
-                                <li>시험</li>
-                                <li>설문</li>
-                            </ul>
-                        </li>
-                        <li>
-                            <span>확습 현황</span>
-                            <ul>
-                                <li>학습 현황</li>
-                            </ul>
-                        </li>
-                        <li>
-                            <span>과목 정보</span>
-                            <ul>
-                                <li>평가 계획서</li>
-                                <li>강의 계획서</li>
-                                <li>과목 정보</li>
-                            </ul>
-                        </li>
-                        <li>
-                            <span>성적</span>
-                            <ul>
-                                <li>성적보기</li>
+                                <li v-for="(item, idx) in menu.items" :key="idx">{{ item }}</li>
                             </ul>
                         </li>
                     </ul>
@@ -62,121 +22,74 @@
                 <div class="bar"></div>
                 <div class="dailyBtn">내 할일보기</div>
             </article>
+    
             <article class="contentBox">
                 <div class="title">
                     <span>
-                        <a href="#">홈</a>
-                        >
-                        <a href="#">강의실</a>
-                        >
-                        <a href="#">데이터사이언스기초</a>
-                        >
+                        <a href="#">홈</a> >
+                        <a href="#">강의실</a> >
+                        <a href="#">데이터사이언스기초</a> >
                         <a href="#">학습 목차</a>
                     </span>
                 </div>
+
                 <div class="contentTab">
                     <span>
                         <span class="select">학습 목차</span>
                         <span>강의 목록</span>
                     </span>
                 </div>
+
                 <div class="content">
                     <div class="expl">
+                    <span>
+                        <span>온라인 학습현황</span>
                         <span>
-                            <span>온라인 학습현황</span>
-                            <span>
-                                - 학습완료: 각 주차의 학습기간 내에 강의 콘텐츠의 출석인정 기준시간 이상 학습한 경우<br>
-                                - 학습중/학습안함: 학습해야 할 강의 콘텐츠의 출석인정 기준시간 만큼 학습을 완료하지 못한 경우로 결석 처리
-                            </span>
+                            - 학습완료: 각 주차의 학습기간 내에 강의 콘텐츠의 출석인정 기준시간 이상 학습한 경우<br />
+                            - 학습중/학습안함: 학습해야 할 강의 콘텐츠의 출석인정 기준시간 만큼 학습을 완료하지 못한 경우로 결석 처리
                         </span>
+                    </span>
+                    <span>
+                        <span>온라인</span>
                         <span>
-                            <span>온라인</span>
-                            <span>
-                                <span class="end"></span> 학습완료
-                                <span class="ing"></span> 학습중
-                                <span class="fuck"></span> 학습안함
-                            </span>
+                            <span class="end"></span> 학습완료
+                            <span class="ing"></span> 학습중
+                            <span class="fuck"></span> 학습안함
                         </span>
+                    </span>
                     </div>
                 </div>
+
                 <div class="table">
                     <table>
                         <thead>
                             <tr>
                                 <th class="fisrt">
                                     온라인 출석률
-                                    <span class="statusBar">
-                                        <span class="bar"></span>
-                                    </span>
+                                    <span class="statusBar"><span class="bar"></span></span>
                                 </th>
-                                <th>
-                                    1주차
-                                    <span>2024-10-05 ~ 2024-10-10</span>
-                                </th>
-                                <th>
-                                    2주차
-                                    <span>2024-10-05 ~ 2024-10-10</span>
-                                </th>
-                                <th>
-                                    3주차
-                                    <span>2024-10-05 ~ 2024-10-10</span>
+                                <th v-for="(week, index) in weekData" :key="index">
+                                    {{ week.week }}
+                                    <span>{{ week.date }}</span>
                                 </th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
                                 <th class="fisrt">강의영상</th>
-                                <td>
+                                <td v-for="(week, index) in weekData" :key="index">
                                     <span>
-                                        <span>가나다라마바사아자차카타파하</span>
-                                        <span class="statusBar end">
-                                            <span class="bar"></span>
-                                        </span>
-                                        <span>교안 다운로드</span>
-                                    </span>
-                                </td>
-                                <td>
-                                    <span>
-                                        <span>뭘봐?</span>
-                                        <span class="statusBar ing">
-                                            <span class="bar"></span>
-                                        </span>
-                                        <span>교안 다운로드</span>
-                                    </span>
-                                </td>
-                                <td>
-                                    <span>
-                                        <span>널봐</span>
-                                        <span class="statusBar fuck">
+                                        <span>{{ week.content }}</span>
+                                        <span :class="`statusBar ${week.status}`">
                                             <span class="bar"></span>
                                         </span>
                                         <span>교안 다운로드</span>
                                     </span>
                                 </td>
                             </tr>
-                            <tr>
-                                <th class="fisrt">공지사항</th>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <th class="fisrt">질의응답</th>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <th class="fisrt">과 제</th>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <th class="fisrt">학습자료실</th>
-                                <td></td>
-                                <td></td>
-                                <td></td>
+                            <tr v-for="(row, idx) in ['공지사항', '질의응답', '과 제', '학습자료실']" :key="idx">
+                                <th class="fisrt">{{ row }}</th>
+                                <td v-for="week in weekData" :key="week.week"></td>
                             </tr>
                         </tbody>
                     </table>
@@ -185,3 +98,40 @@
         </div>
     </section>
 </template>
+
+<script setup>
+
+const menuItems = [
+    {
+        title: "학습 목차",
+        items: ["학습 목차", "강의 목록"],
+    },
+    {
+        title: "학습 정보",
+        items: ["공지사항", "질의응답", "자료공유", "상담게시판"],
+    },
+    {
+        title: "학습 활동",
+        items: ["출석", "과제", "토론", "시험", "설문"],
+    },
+    {
+        title: "확습 현황",
+        items: ["학습 현황"],
+    },
+    {
+        title: "과목 정보",
+        items: ["평가 계획서", "강의 계획서", "과목 정보"],
+    },
+    {
+        title: "성적",
+        items: ["성적보기"],
+    },
+];
+
+const weekData = [
+    { week: "1주차", date: "2024-10-05 ~ 2024-10-10", status: "end", content: "강의 1주차 내용" },
+    { week: "2주차", date: "2024-10-05 ~ 2024-10-10", status: "ing", content: "강의 2주차 내용" },
+    { week: "3주차", date: "2024-10-05 ~ 2024-10-10", status: "fuck", content: "강의 3주차 내용" },
+];
+
+</script>
